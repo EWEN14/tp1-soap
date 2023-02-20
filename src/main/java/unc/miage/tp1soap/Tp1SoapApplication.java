907d6f36b697831a5b411a3ddb85c0f95d020dec
Ewen CLÉMENT <ewenclement@gmail.com>
2023-02-20 14:22:27 +1100

    Initialisation du projet

---
+++ src/main/java/unc/miage/tp1soap/Tp1SoapApplication.java
@@ -0,0 +1,13 @@
+package unc.miage.tp1soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1SoapApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Tp1SoapApplication.class, args);
+  }
+
+}
---
+++ src/test/java/unc/miage/tp1soap/Tp1SoapApplicationTests.java
@@ -0,0 +1,13 @@
+package unc.miage.tp1soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1SoapApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
